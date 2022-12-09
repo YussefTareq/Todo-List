10ca0df8d01ff3a67be711684df0f31497d7370f
YussefTareq <115657000+YussefTareq@users.noreply.github.com>
2022-12-09 21:46:49 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import TodoListFC from"./components/TodoListFC"
+const App = () => {
+    return (
+        <React.Fragment>
+            <TodoListFC/>
+        </React.Fragment>
+    );
+};
+
+export default App;
